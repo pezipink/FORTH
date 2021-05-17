db597a05800f982e3a1177f94abc5ffe6ce02928
pezi <pezipink@googlemail.com>
2021-05-17 08:30:52 +0100

    console wip

---
+++ struct.f
@@ -0,0 +1,10 @@
+CELL 2 / CONSTANT HCELL
+: /allot  here over allot swap erase ;
+\ struct creation
+: sfield  ( struct bytes - <name> )  ( adr - adr+n )
+    create over @ ,  swap +!  does> @ + ;
+: svar cell sfield ;
+: struct variable ;
+: *struct  here swap @ /allot ;
+: sembed  @ sfield ;
+
